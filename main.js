5fdf51080af545d059b984646f56a17e348d8ce9
wingillis <win.gillis@gmail.com>
2015-12-22 17:01:33 -0500

    Updated player model with prototype

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ var game = new Phaser.Game(600,450, Phaser.AUTO, 'Wizycs', {
 })
 
 function preload() {
-  game.load.script('player', './models/player.js')
   game.load.tilemap('test1', './assets/maps/wizycs_temp_map.json', null, Phaser.Tilemap.TILED_JSON)
   game.load.image('tiles', './assets/maps/test1.png')
   game.load.image('collide', './assets/maps/sprite_sheet (6).png')
   game.load.spritesheet('flame', './assets/sprites/fireball.png', 64,64)
   game.load.spritesheet('chars', './assets/sprites/chartiles.png', 32, 32)
   game.load.spritesheet('pika', './assets/sprites/pika.jpg', 16,24)
+  game.load.script('player', './models/player.js')
 }
 
 function create() {
@@ -31,7 +31,8 @@ function create() {
   map.setCollision(106, true, collision)
   // map.setCollisionBetween(1,1000, true, layer)
   layer.resizeWorld()
-  wzrd = game.add.sprite(0,0, 'chars')
+  wzrd = new Player(game, 0, 0)
+  // wzrd = game.add.sprite(0,0, 'chars')
   enemy = game.add.sprite(500, 50, 'pika')
   enemy.animations.add('wr', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 21, true)
   enemy.animations.add('wl', [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0], 21, true)
@@ -39,15 +40,15 @@ function create() {
   game.physics.arcade.enable(enemy)
   enemy.body.gravity.y = 500
 
-  flame = game.add.sprite(wzrd.position.x, wzrd.position.y+16, 'flame')
-  flame.animations.add('fire', [16,17,18,19,20,21,22], 20, true)
-  game.physics.arcade.enable(wzrd)
-  wzrd.frame = 10
-  wzrd.animations.add('left', [21,22,23,22], 5, true)
-  wzrd.animations.add('right', [33,34,35,34], 5, true)
-  wzrd.body.gravity.y = 500
-  wzrd.body.collideWorldBounds = true
-  game.camera.follow(wzrd)
+  // flame = game.add.sprite(wzrd.position.x, wzrd.position.y+16, 'flame')
+  // flame.animations.add('fire', [16,17,18,19,20,21,22], 20, true)
+  // game.physics.arcade.enable(wzrd)
+  // wzrd.frame = 10
+  // wzrd.animations.add('left', [21,22,23,22], 5, true)
+  // wzrd.animations.add('right', [33,34,35,34], 5, true)
+  // wzrd.body.gravity.y = 500
+  // wzrd.body.collideWorldBounds = true
+  // game.camera.follow(wzrd)
   keys = game.input.keyboard.createCursorKeys()
   game.add.text(10,10, 'Arrow keys to move, and you can fly!')
   enemy.state = 'right'
@@ -58,26 +59,22 @@ function update() {
   game.physics.arcade.collide(wzrd, collision)
   game.physics.arcade.collide(enemy, collision)
   if (keys.left.isDown) {
-    wzrd.animations.play('left')
-    wzrd.body.velocity.x = -130
+    wzrd.left()
   } else if (keys.right.isDown) {
-    wzrd.animations.play('right')
-    wzrd.body.velocity.x = 130
+    wzrd.right()
   } else {
-    wzrd.animations.stop()
-    wzrd.frame = 10
-    wzrd.body.velocity.x = 0
+    wzrd.stop()
   }
 
   if (keys.up.isDown) {
-    flame.visible = true
-    flame.position.x = wzrd.position.x - 15
-    flame.position.y = wzrd.position.y + 13
-    flame.play('fire')
-    wzrd.body.velocity.y = -150
+    // flame.visible = true
+    // flame.position.x = wzrd.position.x - 15
+    // flame.position.y = wzrd.position.y + 13
+    // flame.play('fire')
+    wzrd.fly()
   } else {
-    flame.visible = false
-    flame.animations.stop()
+    // flame.visible = false
+    // flame.animations.stop()
   }
 }
 
--- models/Player.js
+++ models/Player.js
@@ -1,6 +1,7 @@
 /**
  * Created by gattra on 12/22/2015.
  */
+'use strict';
 
 var Player = function(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, 'chars')
@@ -8,16 +9,37 @@ var Player = function(game, x, y) {
   this.frame = 10
   this.animations.add('left', [21,22,23,22], 5, true)
   this.animations.add('right', [33,34,35,34], 5, true)
-  this.body.gravity.y = 400
+  this.body.gravity.y = 500
   this.body.collideWorldBounds = true
   this.game.camera.follow(this)
+  this.game.add.existing(this)
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype)
 Player.prototype.constructor = Player
 
 Player.prototype.equip = function() {
-  
+
+}
+
+Player.prototype.left = function() {
+  this.animations.play('left')
+  this.body.velocity.x = -150
+}
+
+Player.prototype.right = function () {
+  this.animations.play('right')
+  this.body.velocity.x = 150
+}
+
+Player.prototype.fly = function () {
+  this.body.velocity.y += -18
+}
+
+Player.prototype.stop = function () {
+  this.animations.stop()
+  this.frame = 10
+  this.body.velocity.x = 0
 }
 
 // Player.CreatePlayer = function() {
@@ -33,8 +55,8 @@ Player.prototype.equip = function() {
 //
 // };
 
-Player.EquipPlayer = function() {
-    flame = game.add.sprite(wzrd.position.x, wzrd.position.y-15, 'flame');
-    flame.animations.add('fireRight', [32,33,34,35,36,37,39], 20, true);
-    flame.animations.add('fireLeft', [0,1,2,3,4,5,6,7], 20, true);
-};
+// Player.EquipPlayer = function() {
+//     flame = game.add.sprite(wzrd.position.x, wzrd.position.y-15, 'flame');
+//     flame.animations.add('fireRight', [32,33,34,35,36,37,39], 20, true);
+//     flame.animations.add('fireLeft', [0,1,2,3,4,5,6,7], 20, true);
+// };
