7ab9d794b1642329554e1a34894306cf02f725ab
wingillis <win.gillis@gmail.com>
2015-12-22 14:23:17 -0500

    Matching gregs project structure

--- main.js
+++ main.js
@@ -13,9 +13,9 @@ function preload() {
   game.load.tilemap('test1', './assets/maps/wizycs_temp_map.json', null, Phaser.Tilemap.TILED_JSON)
   game.load.image('tiles', './assets/maps/test1.png')
   game.load.image('collide', './assets/maps/sprite_sheet (6).png')
-  game.load.spritesheet('flame', './assets/maps/fireball.png', 64,64)
-  game.load.spritesheet('chars', './assets/maps/chartiles.png', 32, 32)
-  game.load.spritesheet('pika', './assets/maps/pika.jpg', 16,24)
+  game.load.spritesheet('flame', './assets/sprites/fireball.png', 64,64)
+  game.load.spritesheet('chars', './assets/sprites/chartiles.png', 32, 32)
+  game.load.spritesheet('pika', './assets/sprites/pika.jpg', 16,24)
 }
 
 function create() {
