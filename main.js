2902d3fb623ca4cdaf99072d0ca871e352b38902
wingillis <win.gillis@gmail.com>
2015-12-21 20:53:55 -0500

    Tile maps work properly. You have to reset the cache every once in awhile to get it to work

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+'use strict'
 
+var map, layer
 
 // Game instantiation
 var game = new Phaser.Game(800,600, Phaser.AUTO, 'Wizycs', {
-
+  preload: preload,
+  create: create
 })
+
+function preload() {
+  game.load.tilemap('test1', './assets/maps/wizycs_temp_map.json', null, Phaser.Tilemap.TILED_JSON)
+  game.load.image('tiles', './assets/maps/test1.png')
+}
+
+function create() {
+  game.stage.backgroundColor = '#5DDCDE'
+  map = game.add.tilemap('test1')
+  map.addTilesetImage('tiles', 'tiles')
+  layer = map.createLayer('world')
+  layer.resizeWorld()
+
+}
